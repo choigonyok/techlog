2ec0e8093a92e945677e19ecd1da3af5963ac57f
choigonyok <achoistic98@naver.com>
2023-08-15 00:27:21 +0900

    refactor : delete unnessesary tables in /mysql/sqls/initialize.sql

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -37,36 +37,3 @@ CREATE TABLE `reply` (
         `writerpw` VARCHAR(255) NOT NULL DEFAULT "",
         `text` TEXT NOT NULL DEFAULT "",
         `commentid` INT NOT NULL DEFAULT 0);
-
-CREATE TABLE `beabouttodelete` (
-        `delete_date` DATETIME DEFAULT '0000-00-00 00:00:00',
-        `connection_id` INT,
-        FOREIGN KEY (`connection_id`) REFERENCES `connection`(`connection_id`) ON DELETE CASCADE);
-
-CREATE TABLE `question` (
-        `question_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        `target_word` VARCHAR(255) NOT NULL,
-        `question_contents` VARCHAR(255) NOT NULL);
-
-CREATE TABLE `answer` (
-        `answer_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        `connection_id` INT NOT NULL,
-        `first_answer` VARCHAR(255) DEFAULT 'not-written',
-        `second_answer` VARCHAR(255) DEFAULT 'not-written',
-        `answer_date` VARCHAR(255) NOT NULL,
-        `question_id` INT,
-        FOREIGN KEY (`question_id`) REFERENCES `question`(`question_id`) ON UPDATE CASCADE ON DELETE CASCADE);
-
-CREATE TABLE `exceptionword` (
-        `exception_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        `connection_id` INT NOT NULL,
-        `except_word` TEXT NOT NULL);
-
-CREATE TABLE `anniversary` (
-        `anniversary_id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        `connection_id` INT NOT NULL,
-        `year` INT NOT NULL,
-        `month` INT NOT NULL,
-        `date` INT NOT NULL,
-        `contents` VARCHAR(255) NOT NULL,
-        `d_day` TINYINT(1) NOT NULL);
