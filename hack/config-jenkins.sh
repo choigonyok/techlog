91e94c644cc9d274a6565d6d3d9323a01a4defbd
최윤석 <achoistic98@naver.com>
2023-11-08 00:10:27 +0900

    Build: Update scripts

--- hack/config-jenkins.sh
+++ hack/config-jenkins.sh
@@ -6,8 +6,10 @@ terraform output -json > output.json
 # 출력된 파일에서 id를 가져와 k8s configMap manifest에 입력
 jq --argjson new_output "$(cat metadata.json)" '.spec.csi.volumeAttributes = $new_output' your-pv.yaml > updated-pv.yaml
 
-# configMap 리소스와 jenkins 오브젝트 생성
-kubectl apply -f configmap.yml
-kubectl apply -f jenkins.yml
+MASTER_NODE_IP=$(jq -r '.master-ip.value' output.json)
+WORKER_NODE1_IP=$(jq -r '.worker1-ip.value' output.json)
+WORKER_NODE2_IP=$(jq -r '.worker2-ip.value' output.json)
 
-# 잘 생성되었는지 확인
+EBS_ID=$(jq -r '.jenkins-ebs-id.value' output.json)
+
+sed -e 's/{ebs-volume-id}/$EBS_ID%/g' template.yml > configmap.yml
