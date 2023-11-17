ef59ac95da61b60c05b1e18740fb325075e84b4a
최윤석 <achoistic98@naver.com>
2023-11-17 23:38:04 +0900

    Refactor: Apply package oriented design in main.go

--- cmd/blog.go
+++ cmd/blog.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"github.com/choigonyok/techlog/pkg/database"
-	"github.com/choigonyok/techlog/pkg/middleware"
-	"github.com/choigonyok/techlog/pkg/router"
 	"github.com/choigonyok/techlog/pkg/server"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 var (
-	databaseDriver   = "mysql"
-	handlerPrefix    = "/api/"
-	listenAddress    = "0.0.0.0:8080" // should be 0.0.0.0, not localhost
-	allowOrigin      = []string{"http://localhost", "http://localhost:3000", "http://frontend", "http://fronend:3000"}
-	allowMethods     = []string{"GET", "POST", "DELETE", "PUT"}
-	allowHeaders     = []string{"Content-type"}
-	allowCredentials = true
+	databaseDriver = "mysql"
 )
 
 func main() {
@@ -31,21 +23,16 @@ func main() {
 	var databaseHost = os.Getenv("DB_HOST")
 	var databaseName = os.Getenv("DB_NAME")
 
-	database := database.NewDatabase(databaseDriver, databasePassword, databaseUser, databasePort, databaseHost, databaseName)
+	database := database.New(databaseDriver, databasePassword, databaseUser, databasePort, databaseHost, databaseName)
 	db, _ := database.Open()
 	defer database.Close(db)
 
-	middleware := &middleware.Middleware{}
-	middleware.AllowConfig(allowOrigin, allowMethods, allowHeaders, allowCredentials)
-
-	router := router.NewRouter(middleware)
-	routes := router.NewRoutes(handlerPrefix)
-	router.SetRoutes(routes)
-	httpHandler := router.GetHTTPHandler()
-
-	server := server.New(httpHandler, listenAddress)
-	err := server.Start()
+	server, err := server.New()
 	if err != nil {
-		fmt.Println("server starting error...", err.Error())
+		log.Fatal("server creating error...", err)
+	}
+	err = server.Start()
+	if err != nil {
+		log.Fatal("server starting error...", err.Error())
 	}
 }
