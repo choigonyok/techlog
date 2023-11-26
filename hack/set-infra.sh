52129c082632f1d65610ef362750f391f66f8a9b
최윤석 <achoistic98@naver.com>
2023-11-27 02:03:45 +0900

    Chore: Update directory structure

--- hack/set-infra.sh
+++ hack/set-infra.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 echo "Create terraform plan file..."
-cd ../eks 
+cd ../terraform
 terraform plan -out terraform.plan
 
 echo "Apply terraform..."
@@ -20,28 +20,28 @@ kubectl create ns devops-system
 kubectl create ns argocd
 
 echo "Create nginx ingress controller..."
-kubectl apply -f ../hack/manifests/ingress-controller.yml
+kubectl apply -f ../manifests/ingress-controller.yml
 HTTP_ARN=$(terraform output target_group_http_arn | sed 's/\//\\\//g')
 # HTTPS_ARN=$(terraform output target_group_https_arn | sed 's/\//\\\//g')
-sed -i '' "s/targetGroupARN: HTTP_ARN/targetGroupARN: $HTTP_ARN/" ../hack/manifests/target-group-binding.yml
-# sed -i '' "s/targetGroupARN: HTTPS_ARN/targetGroupARN: $HTTPS_ARN/" ../hack/manifests/target-group-binding.yml
-kubectl apply -f ../hack/manifests/target-group-binding.yml
-sed -i '' 's/targetGroupARN: .*/targetGroupARN: HTTP_ARN/g' ../hack/manifests/target-group-binding.yml
-# sed -i '' "1,/---/s/targetGroupARN: .*/targetGroupARN: HTTP_ARN/" ../hack/manifests/target-group-binding.yml
+sed -i '' "s/targetGroupARN: HTTP_ARN/targetGroupARN: $HTTP_ARN/" ../manifests/target-group-binding.yml
+# sed -i '' "s/targetGroupARN: HTTPS_ARN/targetGroupARN: $HTTPS_ARN/" ../manifests/target-group-binding.yml
+kubectl apply -f ../manifests/target-group-binding.yml
+sed -i '' 's/targetGroupARN: .*/targetGroupARN: HTTP_ARN/g' ../manifests/target-group-binding.yml
+# sed -i '' "1,/---/s/targetGroupARN: .*/targetGroupARN: HTTP_ARN/" ../manifests/target-group-binding.yml
 
 echo "Deploy test ingress and nginx..."
-kubectl apply -f ../hack/manifests/ingress.yml
-kubectl apply -f ../hack/manifests/nginx-test.yml
+kubectl apply -f ../manifests/ingress.yml
+kubectl apply -f ../manifests/nginx-test.yml
 
 echo "Deploy K8S metric server..."
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
 
 echo "Deploy jenkins as Loadbalancer type service object..."
 FILE_SYSTEM_ID=$(terraform output efs_id | sed s/\"//g )
-sed -i '' "s/fileSystemId: FILE_SYSTEM_ID/fileSystemId: $FILE_SYSTEM_ID/" ../hack/manifests/jenkins.yml
-kubectl apply -f ../hack/manifests/jenkins.yml
+sed -i '' "s/fileSystemId: FILE_SYSTEM_ID/fileSystemId: $FILE_SYSTEM_ID/" ../manifests/jenkins.yml
+kubectl apply -f ../manifests/jenkins.yml
 kubectl patch svc jenkins -n devops-system -p '{"spec": {"type": "ClusterIP"}}'
-sed -i '' "s/fileSystemId: .*/fileSystemId: FILE_SYSTEM_ID/" ../hack/manifests/jenkins.yml
+sed -i '' "s/fileSystemId: .*/fileSystemId: FILE_SYSTEM_ID/" ../manifests/jenkins.yml
 
 echo "Issueing ServiceAccount token..."
 kubectl create token jenkins -n devops-system
